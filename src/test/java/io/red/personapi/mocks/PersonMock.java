91cd02a1741a618c4da403f8c08875260fa4c64d
Erasmo Jr <erasmo.junior@gmail.com>
2023-01-12 22:13:01 -0300

    :test_tube: TEST: Adicionado teste para salvar pessoa

---
+++ src/test/java/io/red/personapi/mocks/PersonMock.java
@@ -0,0 +1,34 @@
+package io.red.personapi.mocks;
+
+import io.red.personapi.controllers.requests.PersonRequest;
+import io.red.personapi.models.Person;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.util.List;
+
+public class PersonMock {
+
+    public static Person createPerson() {
+        return new Person(
+                1L,
+                "Doe",
+                Date.from(Instant.now()),
+                List.of(AddressMock.createAddress())
+        );
+    }
+
+    public static Person createOnlyName() {
+        return new Person(
+                "Doe"
+        );
+    }
+
+    public static PersonRequest request(){
+        return new PersonRequest(
+                "Doe"
+        );
+    }
+
+
+}
