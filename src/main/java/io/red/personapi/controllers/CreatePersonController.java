4cc792c9a7c838be49e839c3a42a368758df9c54
Erasmo Jr <erasmo.junior@gmail.com>
2023-01-12 22:13:42 -0300

    :sparkles: FEAT: Criando pessoa com nome

--- src/main/java/io/red/personapi/controllers/CreatePersonController.java
+++ src/main/java/io/red/personapi/controllers/CreatePersonController.java
@@ -1,2 +1,26 @@
-package io.red.personapi.controllers;public class CreatePersonController {
+package io.red.personapi.controllers;
+
+import io.red.personapi.controllers.requests.PersonRequest;
+import io.red.personapi.controllers.responses.PersonResponse;
+import io.red.personapi.services.CreatePersonService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/person")
+public class CreatePersonController {
+
+    private final CreatePersonService service;
+
+    public CreatePersonController(CreatePersonService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PersonResponse create(@RequestBody PersonRequest request) {
+        return service.create(request)
+                .toResponse();
+    }
 }
--- src/main/java/io/red/personapi/services/CreatePersonService.java
+++ src/main/java/io/red/personapi/services/CreatePersonService.java
@@ -2,23 +2,23 @@ package io.red.personapi.services;
 
 import io.red.personapi.PersonRepository;
 import io.red.personapi.controllers.requests.PersonRequest;
-import io.red.personapi.controllers.responses.PersonResponse;
 import io.red.personapi.models.Person;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CreatePerson {
+public class CreatePersonService {
     private final PersonRepository repository;
 
-    public CreatePerson(PersonRepository repository) {
+    public CreatePersonService(PersonRepository repository) {
         this.repository = repository;
     }
 
-    public PersonResponse create(PersonRequest request) {
-        Person newPerson = new Person(
-                request.name()
+    public Person create(PersonRequest request) {
+        final var namePerson = request.name();
+
+        final var newPerson = new Person(
+                namePerson
         );
-        return repository.save(newPerson)
-                .toResponse();
+        return repository.save(newPerson);
     }
 }
