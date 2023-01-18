b0091f9fdb247e9f6082f6c996f9624ca5ba02a6
Erasmo Jr <erasmo.junior@gmail.com>
2023-01-17 23:27:46 -0300

    :test_tube: TEST: CreatePerson Test added

--- src/test/java/io/red/personapi/mocks/AddressMock.java
+++ src/test/java/io/red/personapi/mocks/AddressMock.java
@@ -1,5 +1,6 @@
 package io.red.personapi.mocks;
 
+import io.red.personapi.controllers.requests.PersonRequest;
 import io.red.personapi.models.Address;
 
 public class AddressMock {
@@ -7,6 +8,15 @@ public class AddressMock {
     public static Address createAddress(){
         return new Address(
                 1L,
+                "Street",
+                "3558565",
+                "Number",
+                "City"
+        );
+    }
+
+    public static PersonRequest.Address createAddressRequest(){
+        return new PersonRequest.Address(
                 "Street",
                 "3558565",
                 "Number",
