12bdeb8c6670da7adb117fc51020280bda87dcbe
Erasmo Jr <erasmo.junior@gmail.com>
2023-01-17 22:11:24 -0300

    :sparkles: FEAT: Logger for debugging

--- src/main/java/io/red/personapi/services/CreatePersonService.java
+++ src/main/java/io/red/personapi/services/CreatePersonService.java
@@ -5,6 +5,8 @@ import io.red.personapi.models.Address;
 import io.red.personapi.models.Person;
 import io.red.personapi.repositories.AddressRepository;
 import io.red.personapi.repositories.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
@@ -15,6 +17,7 @@ import java.util.List;
 public class CreatePersonService {
     private final PersonRepository personRepository;
     private final AddressRepository addressRepository;
+    private final static Logger LOGGER = LoggerFactory.getLogger(CreateAddressPersonService.class);
 
     public CreatePersonService(PersonRepository personRepository,
                                AddressRepository addressRepository) {
@@ -42,6 +45,7 @@ public class CreatePersonService {
                 birthDate,
                 addressList
         );
+        LOGGER.info("Person {} created with {} address(es)",namePerson, addressList.size());
         return personRepository.save(newPerson);
     }
 }
